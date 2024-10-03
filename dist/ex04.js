bb328b5c22490664371ea51b2953cc576baf85e4
Nao Gobaru <take57bamboo@gmail.com>
2024-10-02 19:47:30 -0700

    add solution

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,14 +1,12 @@
+"use strict";
 // 4. Create a function named `findMax` that takes an array of numbers as a parameter.
 //    The function should return the largest number in the array.
 //    Ensure the parameter and return type are typed.
-
-function findMax(numbers: number[]): number {
-    const aryMax = function (a: number, b: number) {return Math.max(a, b);}
-
-    const result = numbers.reduce(aryMax)
-    return result
+function findMax(numbers) {
+    const aryMax = function (a, b) { return Math.max(a, b); };
+    const result = numbers.reduce(aryMax);
+    return result;
 }
-
 // Expected output:
 console.log(findMax([1, 5, 3, 9, 2])); // 9
-console.log(findMax([7, 2, 10, 4]));   // 10
+console.log(findMax([7, 2, 10, 4])); // 10
--- dist/ex05.js
+++ dist/ex05.js
@@ -1,16 +1,15 @@
+"use strict";
 // 5. Create a function named `countVowels` that takes a string as a parameter.
 //    The function should return the number of vowels (a, e, i, o, u) in the string.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function countVowels(word: string): number {
-    const words = word.split("")
-    const vowels: any = ["a", "e", "i", "o", "u"]
+function countVowels(word) {
+    const words = word.split("");
+    const vowels = ["a", "e", "i", "o", "u"];
     const letters = words.filter(value => {
-        return vowels.includes(value)
-    })
-    return letters.length
+        return vowels.includes(value);
+    });
+    return letters.length;
 }
-
 // Expected output:
-console.log(countVowels("hello"));    // 2
-console.log(countVowels("typescript"));// 3
+console.log(countVowels("hello")); // 2
+console.log(countVowels("typescript")); // 3
